495d1c29e47551fddaa182a199ac56dfe799487e
Quoc Duong <Quocduong.th10b.mta@gmail.com>
2022-09-25 21:50:18 +0700

    Add and update file

---
+++ C1 - Azure Infrastructure Operations/project/starter_files/variables.tf
@@ -0,0 +1,19 @@
+variable "prefix" {
+  description = "The prefix which should be used for all resources in this example"
+  default = "duongnq9-project1"
+}
+
+variable "location" {
+  description = "The Azure Region in which all resources in this example should be created."
+  default = "centralus"
+}
+
+variable "username" {
+  description = "Enter Username"
+  default = "duongnq9"
+}
+
+variable "password" {
+  description = "Enter Password"
+  default = "Fsoft@12345678"
+}
